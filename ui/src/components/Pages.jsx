1b35263255a1d5e2bfaacd6a0ccf33366d750bae
Stylian <stylianos.chatzimanolis@dotmatics.com>
2019-11-14 16:12:41 +0200

    tab right click on them to edit/delete
    only menu added

--- ui/src/components/Pages.jsx
+++ ui/src/components/Pages.jsx
@@ -1,7 +1,22 @@
 import React, {Component} from 'react';
-import {Button, Dialog, DialogActions, DialogContent, DialogTitle, Tab, Tabs, TextField} from "@material-ui/core";
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    ListItemIcon,
+    ListItemText,
+    Tab,
+    Tabs,
+    TextField
+} from "@material-ui/core";
 import AddIcon from '@material-ui/icons/Add';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
 import Page from "./Page";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
 class Pages extends Component {
 
@@ -16,6 +31,11 @@ class Pages extends Component {
             title: "",
             titleError: true,
             description: "",
+            editMenu: {
+                x: 0,
+                y: 0,
+                obj: null
+            },
         };
     }
 
@@ -44,74 +64,7 @@ class Pages extends Component {
             )
     }
 
-    closeDialog = (event, newValue) => {
-        this.setState(state => {
-            return {
-                ...state,
-                dialogOpen: false,
-            }
-        });
-    }
-
-    insertPage = (event) => {
-
-        fetch("/pages/", {
-            method: 'POST',
-            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-            body: "title=" + this.state.title + "&description=" + this.state.description
-        })
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    let newPage = result;
-                    if (newPage.id === -1) {
-                        return;
-                    }
-                    let newPages = [...this.state.pages, newPage]
-                    this.setState(state => {
-                        return {
-                            ...state,
-                            pages: newPages,
-                            dialogOpen: false,
-                            tabActive: this.state.pages.length // as it has not updated yet, so that is former size
-                        }
-                    });
-        },
-        (error) => {
-            this.setState(state => {
-                return {
-                    ...state,
-                    error
-                }
-            });
-        }
-        )
-
-    }
-
-    handleChange = (event, newValue) => {
-
-        let isTab = event.currentTarget.dataset.istab === "true";
-        // plus icon popup
-        if(!isTab) {
-            this.setState(state => {
-                return {
-                    ...state,
-                    dialogOpen: true,
-                }
-            });
-        }else {
-            // tab switch
-            this.setState(state => {
-                return {
-                    ...state,
-                    tabActive: newValue,
-                }
-            });
-        }
-
-    }
-
+    // create page dialog handlers
     changeField = field => (event) => {
         let value = event.target.value;
         if (value < 0) {
@@ -137,16 +90,110 @@ class Pages extends Component {
 
     }
 
+    closeDialog = (event, newValue) => {
+        this.setState(state => {
+            return {
+                ...state,
+                dialogOpen: false,
+            }
+        });
+    }
+
+    // main page handlers
+    insertPage = (event) => {
+
+        fetch("/pages/", {
+            method: 'POST',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            body: "title=" + this.state.title + "&description=" + this.state.description
+        })
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    let newPage = result;
+                    if (newPage.id === -1) {
+                        return;
+                    }
+                    let newPages = [...this.state.pages, newPage]
+                    this.setState(state => {
+                        return {
+                            ...state,
+                            pages: newPages,
+                            dialogOpen: false,
+                            tabActive: this.state.pages.length // as it has not updated yet, so that is former size
+                        }
+                    });
+                },
+                (error) => {
+                    this.setState(state => {
+                        return {
+                            ...state,
+                            error
+                        }
+                    });
+                }
+            )
+
+    }
+
+    handleChange = (event, newValue) => {
+
+        let isTab = event.currentTarget.dataset.istab === "true";
+        // plus icon popup
+        if (!isTab) {
+            this.setState(state => {
+                return {
+                    ...state,
+                    dialogOpen: true,
+                }
+            });
+        } else {
+            // tab switch
+            this.setState(state => {
+                return {
+                    ...state,
+                    tabActive: newValue,
+                }
+            });
+        }
+
+    }
+
+    // edit menu handlers
+    editMenuClose = () => {
+        this.setState({
+            editMenu: {
+                obj: null
+            }
+        });
+    }
+
+    editMenuOpen = (event) => {
+        this.editMenuClose();
+        if (event.buttons === 2) {
+            document.oncontextmenu = function () {
+                return false;
+            }
+            this.setState({
+                editMenu: {
+                    obj: event.currentTarget,
+                    x: event.clientX,
+                    y: event.clientY
+                }
+            });
+        }
+    }
+
     render() {
         return this.state.isLoaded ? (
                 <div>
                     <Tabs value={this.state.tabActive} onChange={this.handleChange}>
                         {this.state.pages.map((page, k) => {
                             return (
-                                <Tab label={page.title} data-istab={true} key={k}/>
+                                <Tab label={page.title} data-istab={true} key={k} onMouseDown={this.editMenuOpen}/>
                             )
                         })}
-                        <Tab icon={<AddIcon />} data-istab={false}/>
+                        <Tab icon={<AddIcon/>} data-istab={false}/>
                     </Tabs>
                     {this.state.pages.map((page, k) => {
                         return (
@@ -154,7 +201,30 @@ class Pages extends Component {
                         )
                     })}
 
+                    {/* edit page menu */}
+                    <Menu
+                        id="simple-menu"
+                        anchorReference="anchorPosition"
+                        anchorPosition={{top: this.state.editMenu.y, left: this.state.editMenu.x}}
+                        keepMounted
+                        open={Boolean(this.state.editMenu.obj)}
+                        onClose={this.editMenuClose}
+                    >
+                        <MenuItem>
+                            <ListItemIcon>
+                                <EditIcon/>
+                            </ListItemIcon>
+                            <ListItemText primary="Edit"/>
+                        </MenuItem>
+                        <MenuItem>
+                            <ListItemIcon>
+                                <DeleteIcon/>
+                            </ListItemIcon>
+                            <ListItemText primary="Delete"/>
+                        </MenuItem>
+                    </Menu>
 
+                    {/*create page dialog*/}
                     <Dialog open={this.state.dialogOpen} onClose={this.closeDialog} aria-labelledby="form-dialog-title">
                         <DialogTitle id="form-dialog-title">add a new page</DialogTitle>
                         <DialogContent>
